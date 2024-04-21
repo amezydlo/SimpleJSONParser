5c1eaf6fb5b36faf268349c3ec56b924fcfd57cb
amezydlo <adamkmezydlo@gmai.com>
2024-04-21 22:03:38 +0200

    optimise import

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -2,8 +2,6 @@ package org.example;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
 
 public class Application {
     public static void main(String[] args) {
